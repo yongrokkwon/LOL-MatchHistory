ab7b904a2b8bde13a68ff38b4493d32cb4761f26
yongrokkwon <yongrok.kwon@gmail.com>
2023-02-27 22:56:23 +0900

    Add Hilt Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -62,6 +64,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.3.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
     implementation("androidx.compose.material3:material3:1.0.1")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    //
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ subprojects {
 plugins {
     id("com.github.ben-manes.versions") version "0.43.0"
     id("nl.littlerobots.version-catalog-update") version "0.7.0"
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 //    id 'com.android.application' version '7.4.1' apply false
 //    id 'com.android.library' version '7.4.1' apply false
 //    id 'org.jetbrains.kotlin.android' version '1.7.0' apply false
