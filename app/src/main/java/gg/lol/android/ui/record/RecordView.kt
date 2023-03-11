b83ba929625044d07e9a88526d46b4344611b22c
yongrokkwon <yongrok.kwon@gmail.com>
2023-03-11 16:54:11 +0900

    Remove RecordView Unused Code

--- app/src/main/java/gg/lol/android/ui/record/RecordView.kt
+++ app/src/main/java/gg/lol/android/ui/record/RecordView.kt
@@ -1,16 +1,24 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package gg.lol.android.ui.record
 
 import android.app.Activity
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
