3ded00f65638ac8e6997de6b2c5ddfabcfd0fd7a
yongrokkwon <yongrok.kwon@gmail.com>
2023-03-20 04:15:24 +0900

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,21 @@ buildscript {
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
     repositories {
         google()
         mavenCentral()
     }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
 }
 
 plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     id("com.github.ben-manes.versions") version "0.43.0"
     id("nl.littlerobots.version-catalog-update") version "0.7.0"
     id("com.google.dagger.hilt.android") version "2.44" apply false
@@ -24,7 +32,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.android.dynamic-feature") version "7.4.1" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
-//    id 'com.android.application' version '7.4.1' apply false
-//    id 'com.android.library' version '7.4.1' apply false
-//    id 'org.jetbrains.kotlin.android' version '1.7.0' apply false
 }
