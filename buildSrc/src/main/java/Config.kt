079c4896a0100298cf85bcf778a61eca9a797e52
yongrokkwon <yongrok.kwon@gmail.com>
2023-03-19 23:24:01 +0900

    Modify BaseURL

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -10,15 +10,15 @@ object Config {
         const val APPLICATION_ID = "gg.lol.android"
         const val VERSION_CODE = 1
         const val VERSION_NAME = "1.0.0"
-        const val BASEURL = "https://kr.api.riotgames.com/lol/summoner/v4/"
+        const val BASEURL = "https://kr.api.riotgames.com"
     }
 
     object Debug {
-        const val BASEURL = "https://kr.api.riotgames.com/lol/summoner/v4/"
+        const val BASEURL = "https://kr.api.riotgames.com"
     }
 
     object Staging {
-        const val BASEURL = "https://kr.api.riotgames.com/lol/summoner/v4/"
+        const val BASEURL = "https://kr.api.riotgames.com"
     }
 
     object ComposeOption {
