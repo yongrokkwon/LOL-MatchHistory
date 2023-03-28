b28c55ebc9dc5a7e77b85e28bd13fde9021b8b84
yongrokkwon <yongrok.kwon@gmail.com>
2023-03-28 18:36:44 +0900

    Remove Legacy

--- app/src/main/java/gg/lol/android/network/NetworkException.kt
+++
@@ -1,5 +0,0 @@
-package gg.lol.android.network
-
-import java.io.IOException
-
-class NetworkException(code: Int, message: String) : IOException(message)
--- app/src/main/java/gg/lol/android/network/Result.kt
+++
@@ -1,6 +0,0 @@
-package gg.lol.android.network
-
-sealed class Result<out T : Any> {
-    data class Success<out T : Any>(val data: T) : Result<T>()
-    data class Error(val exception: Exception? = null) : Result<Nothing>()
-}
