5aa6f2bac33e8e4796558fb393d0a079fa6de6dd
yongrokkwon <yongrok.kwon@gmail.com>
2023-04-18 17:13:32 +0900

    MySummoner TODO

--- app/src/main/java/gg/lol/android/ui/home/HomeView.kt
+++ app/src/main/java/gg/lol/android/ui/home/HomeView.kt
@@ -7,13 +7,29 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Info
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -100,8 +116,9 @@ fun RenderView(navController: NavController, viewModel: HomeViewModel) {
             contentScale = ContentScale.FillBounds
         )
         FavoriteSummonerView(navController, viewModel)
-        mySummoner?.let { MySummonerView(mySummoner) }
-            ?: MySummonerEmptyView(navController)
+        // TODO
+//        mySummoner?.let { MySummonerView(mySummoner) }
+//            ?: MySummonerEmptyView(navController)
     }
 }
 
