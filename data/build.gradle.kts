f6a511193eb4586c3b4f77d50b64c925f339d5bd
yongrokkwon <yongrok.kwon@gmail.com>
2023-04-20 23:46:20 +0900

    Refactor Data Module build.gradle.kts legacy

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
 }
@@ -11,26 +11,15 @@ properties.load(project.rootProject.file("local.properties").inputStream())
 
 android {
     namespace = "gg.op.lol"
-    compileSdk = 33
+    compileSdk = Config.Android.COMPILESDK
 
     defaultConfig {
-        minSdk = 26
-        targetSdk = 33
+        minSdk = Config.Android.MINSDK
+        targetSdk = Config.Android.TARGETSDK
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
 
         buildConfigField("String", "API_KEY", properties["API_KEY"].toString())
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
